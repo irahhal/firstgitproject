740514f186527d5dc47c22ccd6cf77cc223ecbcb
irahhal <imad.rahal@gmail.com>
2016-02-09 12:54:15 -0600

    trying out stuff

--- Driver.java
+++ Driver.java
@@ -9,11 +9,11 @@ public class Driver
    {  
       // because of the import statement, we don't have to use
       // edu.csbsju.cs.Employee here
-      Employee harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+      Employee harry2 = new Employee("Harry Hacker", 50000, 1989, 10, 1);
 
       // print out information about harry
       // use java.lang.System.out here
-      out.println(harry);
+      out.println(harry2);
    }
 }
 
