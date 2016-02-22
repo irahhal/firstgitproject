f9200276ffa90b3fb0d172a41e881fe92b42c357
irahhal <imad.rahal@gmail.com>
2016-02-22 14:00:49 -0600

    I don't want to

--- Driver.java
+++ Driver.java
@@ -7,13 +7,7 @@ public class Driver
 {  
    public static void main(String[] args)
    {  
-      // because of the import statement, we don't have to use
-      // edu.csbsju.cs.Employee here
-      Employee harry2 = new Employee("Harry Hacker", 50000, 1989, 10, 1);
 
-      // print out information about harry
-      // use java.lang.System.out here
-      out.println(harry2);
    }
 }
 
